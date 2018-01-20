302ec23b1232df660137edcc278267b04c814cca
galiulin <galiulin.arthur@gmail.com>
2018-01-20 12:15:38 +0300

    добавлена зависимость на log4j в качестве примера подкорректирован helloServlet добавлен конфигурационный файл log4j cкорректирован .gitignore

--- src/main/java/servlets/HelloServlet.java
+++ src/main/java/servlets/HelloServlet.java
@@ -1,5 +1,7 @@
 package servlets;
 
+import org.apache.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,9 +9,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
+
+    private static final Logger logger = Logger.getLogger(HelloServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.debug("doGet start");
         resp.getWriter().println("hello");
+        logger.debug("doGet end");
     }
 
     @Override
