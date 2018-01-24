5d8776e2647cf6f782e3d53980c70296feed2f05
galiulin <galiulin.arthur@gmail.com>
2018-01-24 11:21:59 +0300

    добавлен Tomcat JDBC Connection Pool

--- src/main/java/connection/ConnectionDBImpl.java
+++ src/main/java/connection/ConnectionDBImpl.java
@@ -3,6 +3,8 @@ package connection;
 import dao.UserDAO;
 import dao.UserDAOImpl;
 import org.apache.log4j.Logger;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
 import pojo.User;
 
 import java.sql.*;
@@ -11,44 +13,59 @@ import java.util.function.Function;
 
 public class ConnectionDBImpl implements ConnectionDB {
     private static final ConnectionDB INSTANCE = new ConnectionDBImpl();
-    private static final Integer PORT = 5432;
+    private static final int PORT = 5432;
     private static final String DB = "taskdairy";
     private static final String CONNECTION_URL = "jdbc:postgresql://localhost:%d/%s";
-    private static String user = "admin";
-    private static String pass = "admin";
-
+    private static final String user = "admin";
+    private static final String pass = "admin";
     private static Logger logger = Logger.getLogger(ConnectionDBImpl.class);
+    private final DataSource dataSource;
 
     private ConnectionDBImpl() {
+        PoolProperties p = new PoolProperties();
+        p.setUrl(String.format(CONNECTION_URL, PORT, DB));
+        p.setDriverClassName("org.postgresql.Driver");
+        p.setUsername(user);
+        p.setPassword(pass);
+        p.setJmxEnabled(true);
+        p.setTestWhileIdle(false);
+        p.setTestOnBorrow(true);
+        p.setValidationQuery("SELECT 1");
+        p.setTestOnReturn(false);
+        p.setValidationInterval(30000);
+        p.setTimeBetweenEvictionRunsMillis(30000);
+        p.setMaxActive(100);
+        p.setInitialSize(10);
+        p.setMaxWait(10000);
+        p.setRemoveAbandonedTimeout(60);
+        p.setMinEvictableIdleTimeMillis(30000);
+        p.setMinIdle(10);
+        p.setLogAbandoned(true);
+        p.setRemoveAbandoned(true);
+        p.setJdbcInterceptors(
+                "org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;" +
+                        "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
+        dataSource = new DataSource();
+        dataSource.setPoolProperties(p);
     }
 
-    public static ConnectionDB getInstance(){
+    public static ConnectionDB getInstance() {
         return INSTANCE;
     }
 
-    private static Connection getConnection() {
+    public Connection getConnect() {
         Connection connection = null;
         try {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(
-                    String.format(CONNECTION_URL, PORT, DB),
-                    user,
-                    pass);
+            connection = dataSource.getConnection();
         } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            logger.warn(e.getMessage(), e);
         }
         return connection;
-    }
-    
-    public Connection getConnect(){
-        return getConnection();
     }
 
     public <T> T getFromDB(FunctionSQL<Connection, T> function) throws SQLException {
         T obj = null;
-        try (Connection connection = getConnection()){
+        try (Connection connection = getConnect()) {
             obj = function.apply(connection);
         } catch (SQLException e) {
 //            e.printStackTrace();
